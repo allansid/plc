27e1124014a2543a31b3d8107c3a9e997d15f070
diogo <dwsb@cin.ufpe.br>
2017-11-30 19:13:42 -0300

    estrutura do projeto

---
+++ Jogo/src/classesAuxiliares/Posicao.java
@@ -0,0 +1,29 @@
+package classesAuxiliares;
+
+public class Posicao {
+	
+	private double x;
+	private double y;
+	
+	public Posicao(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public synchronized void setX(double x) {
+		this.x = x;
+	}
+	
+	public synchronized void setY(double y) {
+		this.y = y;
+	}
+	
+	public double getX() {
+		return this.x;
+	}
+	
+	public double getY() {
+		return this.y;
+	}
+
+}
